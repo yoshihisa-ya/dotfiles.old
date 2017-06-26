865bb37d936891d4d64c41e615fd045e194bbe9c
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2017-06-26 09:22:44 +0900

    Add tmux status line settings for apple mac book pro

--- tmux-script/load-average.sh
+++ tmux-script/load-average.sh
@@ -3,4 +3,7 @@
 if [ $OSTYPE == 'linux-gnu' ]; then
   echo -n ' '
   cat /proc/loadavg | cut -d " " -f 1-3
+elif [ $OSTYPE == 'darwin16' ]; then
+  echo -n ' '
+  uptime | awk '{print $(NF-2),$(NF-1),$NF}'
 fi
--- tmux-script/sensors.sh
+++ tmux-script/sensors.sh
@@ -3,4 +3,9 @@
 if [ $OSTYPE == 'linux-gnu' ]; then
   echo -n ' '
   sensors | grep "Package id 0:" | cut -c 16-23
+elif [ $OSTYPE == 'darwin16' ]; then
+  echo -n ' '
+  istats cpu temp | cut -b11-18 | tr '\n' ', '
+  echo -n 'Battery: '
+  istats battery charge | cut -b17-20
 fi
