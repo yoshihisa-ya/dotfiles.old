0d2067a5a364f83d545ba7f1d5cb244d850d6d22
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2017-06-27 23:56:49 +0900

    Fix linux nowplaying

--- tmux-script/now-playing.sh
+++ tmux-script/now-playing.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if [ -f /usr/bin/rhythmbox-client ]; then
-  echo -n ' '
-  rhythmbox-client --no-start --print-playing --print-playing-format='NowPlaying: %tt Singer: %ta'
+if [ $OSTYPE == 'linux-gnu' ]; then
+  if [ -f /usr/bin/rhythmbox-client ]; then
+    echo -n ' '
+    rhythmbox-client --no-start --print-playing --print-playing-format='NowPlaying: %tt Singer: %ta'
+  fi
 fi
 
-CURRENT_TRACK=$(osascript <<EOF
-tell application "iTunes"
-  return "NowPlaying: " & name of current track & " Singer: " & artist of current track
-end tell
-EOF)
+if [ $OSTYPE == 'darwin16' ]; then
+  CURRENT_TRACK=$(osascript <<EOF
+  tell application "iTunes"
+    return "NowPlaying: " & name of current track & " Singer: " & artist of current track
+  end tell
+  EOF)
 
-echo $CURRENT_TRACK
+  echo $CURRENT_TRACK
+fi
+
+
