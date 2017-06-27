f64b4938f723daddeffbf29425e40e905da53aaf
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2017-06-27 22:20:05 +0900

    Add now playing tmux status for mac

--- tmux-script/now-playing.sh
+++ tmux-script/now-playing.sh
@@ -2,5 +2,13 @@
 
 if [ -f /usr/bin/rhythmbox-client ]; then
   echo -n ' '
-  rhythmbox-client --no-start --print-playing --print-playing-format='NowPlaying: %tt/%at Singer: %ta'
+  rhythmbox-client --no-start --print-playing --print-playing-format='NowPlaying: %tt Singer: %ta'
 fi
+
+CURRENT_TRACK=$(osascript <<EOF
+tell application "iTunes"
+  return "NowPlaying: " & name of current track & " Singer: " & artist of current track
+end tell
+EOF)
+
+echo $CURRENT_TRACK
