9c4691ddd6ef352894704a330e58b006858fef23
Yoshihisa Yamano <yoshihisa.yamano@gmail.com>
2019-08-10 07:56:38 +0900

    Update tmux status line

--- tmux-script/load-average.sh
+++ tmux-script/load-average.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [ $OSTYPE == 'linux-gnu' ]; then
-  echo -n ' '
-  cat /proc/loadavg | cut -d " " -f 1-3
-elif [ $OSTYPE == 'darwin16' ]; then
-  echo -n ' '
-  uptime | awk '{print $(NF-2),$(NF-1),$NF}'
+    echo -n ' '
+    cat /proc/loadavg | cut -d " " -f 1-3
 fi
--- tmux-script/sensors.sh
+++ tmux-script/sensors.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ $OSTYPE == 'linux-gnu' ]; then
-  echo -n ' '
-  sensors | grep "Package id 0:" | cut -c 16-23
-elif [ $OSTYPE == 'darwin16' ]; then
-  echo -n ' '
-  istats cpu temp | cut -b11-18 | tr '\n' ', '
-  echo -n 'Battery: '
-  istats battery charge | cut -b17-20
+if [ $HOSTNAME == 'dt.yoshihisa.link' ]; then
+    echo -n ' '
+    sensors | grep "Package id 0:" | cut -c 16-23
 fi
